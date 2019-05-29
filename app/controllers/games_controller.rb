5bf39c03014b03d5af2ea1cdfb5049fb66fb1d99
Kayode Adeniyi <kayode.adeniyi@andela.co>
2019-05-29 19:28:00 +0100

    Remove unused controller methods

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,14 +1,8 @@
 class GamesController < ApplicationController
-  before_action :set_game, only: [:show, :update, :destroy]
 
   # GET /games
   def index
     @games = Game.all.includes(:winner)
-  end
-
-  # GET /games/1
-  def show
-    render json: @game
   end
 
   # POST /games
@@ -22,25 +16,7 @@ class GamesController < ApplicationController
     end
   end
 
-  # PATCH/PUT /games/1
-  def update
-    if @game.update(game_params)
-      render json: @game
-    else
-      render json: @game.errors, status: :unprocessable_entity
-    end
-  end
-
-  # DELETE /games/1
-  def destroy
-    @game.destroy
-  end
-
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_game
-      @game = Game.find(params[:id])
-    end
 
     # Only allow a trusted parameter "white list" through.
     def game_params
